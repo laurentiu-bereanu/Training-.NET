5099377de10551a053a4525f584fe9df99bf4304
laurentiu-bereanu <laurentiu.bereanu97@gmail.com>
2024-11-27 18:15:56 +0200

    Add project files.

---
+++ TrainingHomeworks/Tema1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingHomeworks.Tema1
+{
+    public class Person
+    {
+
+        public Person() { }
+
+        public String FirstName { get; set; } = string.Empty;
+
+        public String LastName { get; set; } = string.Empty;
+
+        public String Gender { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+    }
+}
